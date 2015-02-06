ad7727085c783234abc57a6981d96084a4d9543d
drumilbakhai <drums.b123@gmail.com>
2015-02-07 03:26:48 +0530

    Third
    pushwoosh implemented

--- www/js/index.js
+++ www/js/index.js
@@ -16,6 +16,20 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
+function initPushwoosh() {
+	var pushNotification = window.plugins.pushNotification;
+	if(device.platform == "Android")
+	{
+		registerPushwooshAndroid();
+	}
+
+	if(device.platform == "iPhone" || device.platform == "iOS")
+	{
+		registerPushwooshIOS();
+	}
+}
+
 var app = {
     // Application Constructor
     initialize: function() {
@@ -31,8 +45,9 @@ var app = {
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
+    // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
+        initPushwoosh();
         app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
@@ -47,5 +62,3 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
-
-app.initialize();
